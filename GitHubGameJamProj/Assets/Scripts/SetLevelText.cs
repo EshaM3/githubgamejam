c67202833d6d4abe1a0f3dfcf6c95661253e5420
snuffysam <bookboy42@gmail.com>
2023-11-30 17:30:58 -0500

    Added fade out for level text

--- GitHubGameJamProj/Assets/Scripts/SetLevelText.cs
+++ GitHubGameJamProj/Assets/Scripts/SetLevelText.cs
@@ -7,7 +7,9 @@ using TMPro;
 public class SetLevelText : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro;
+    public Image panel;
     LevelOrder levelOrder;
+    float timer;
     // Update is called once per frame
     void Update()
     {
@@ -15,7 +17,13 @@ public class SetLevelText : MonoBehaviour
             levelOrder = FindObjectOfType<LevelOrder>();
         }
         if (levelOrder != null){
-            textMeshPro.text = "" + (levelOrder.LevelIndex()+1);
+            textMeshPro.text = "Level " + (levelOrder.LevelIndex()+1);
+        }
+
+        timer += Time.deltaTime;
+        if (timer > 5f){
+            panel.color = new Color(panel.color.r, panel.color.g, panel.color.b, panel.color.a-Time.deltaTime);
+            textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.g, textMeshPro.color.b, textMeshPro.color.a-Time.deltaTime);
         }
     }
 }
